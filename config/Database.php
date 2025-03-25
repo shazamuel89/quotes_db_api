d2e5a3417281fbf88db8cfa01cebfa7862b2789a
Samuel Heinrich <shazamuel89@gmail.com>
2025-03-24 21:33:25 -0500

    Fixed lines after removing port environment variable

--- config/Database.php
+++ config/Database.php
@@ -3,7 +3,6 @@
     class Database {
         
         private $host;
-        private $port;
         private $dbname;
         private $username;
         private $password;
@@ -17,12 +16,11 @@
         }
 
         public function connect() {
-            // instead of $this->conn = null;
             if ($this->conn) {
                 // connection already exists, return it
                 return $this->conn;
             } else {
-                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+                $dsn = "pgsql:host={$this->host};dbname={$this->dbname};";
                 try {
                     $this->conn = new PDO($dsn, $this->username, $this->password);
                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
